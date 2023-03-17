60ba7355a773fb59d4b6be912f1ffe1fd0ce4cab
Xander Lubbe <x.lubbe@icloud.com>
2023-03-17 09:52:46 +0200

    Project initialised.

---
+++ src/main/java/za/co/bbd/pointscalculator/DiscoveryVitalityPointsCalculatorApplication.java
@@ -0,0 +1,13 @@
+package za.co.bbd.pointscalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryVitalityPointsCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryVitalityPointsCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/bbd/pointscalculator/DiscoveryVitalityPointsCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.bbd.pointscalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryVitalityPointsCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
