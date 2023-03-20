d08a72bb18913cb9584aa4730db11c3be18a67b3
Xander Lubbe <x.lubbe@icloud.com>
2023-03-20 11:34:17 +0200

    Updated models.

--- src/main/java/za/co/bbd/pointscalculator/model/ResponsePoints.java
+++ src/main/java/za/co/bbd/pointscalculator/model/ResponsePoints.java
@@ -4,22 +4,10 @@ import lombok.Getter;
 
 @Getter
 public class ResponsePoints {
-    private final boolean visitedDietician;
-    private final int healthyFoodSpend;
-    private final boolean firstHIVTest;
-    private final boolean bloodGlucose;
-    private final int cyclingRaceEventOption1;
     
-
-
-
-    public ResponsePoints(boolean visitedDietician, int healthyfoodSpend, boolean firstHIVTest, boolean bloodGlucose, int cyclingRaceEventOption1){
-        this.visitedDietician = visitedDietician;
-        this.healthyFoodSpend = healthyfoodSpend;
-        this.firstHIVTest = firstHIVTest;
-        this.bloodGlucose = bloodGlucose;
-        this.cyclingRaceEventOption1 = cyclingRaceEventOption1;
-
-
+    private final int points;
+    
+    public ResponsePoints(int points){
+        this.points = points;
     }
 }
