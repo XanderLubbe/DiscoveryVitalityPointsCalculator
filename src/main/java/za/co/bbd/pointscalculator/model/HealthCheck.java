20fec09ebbdbbf0d7768f9c02042503e1617a875
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2023-03-21 10:27:37 +0200

    Added scaffolding for Points repository

---
+++ src/main/java/za/co/bbd/pointscalculator/model/HealthCheck.java
@@ -0,0 +1,22 @@
+package za.co.bbd.pointscalculator.model;
+
+public record HealthCheck(boolean vitalityAge,
+                            int mentalWellbeing,
+                            int golfPlayed,
+                            boolean fluVaccine,
+                            boolean initialHIVTest,
+                            boolean annualHIVTest,
+                            boolean papSmear,
+                            boolean mammogram,
+                            boolean colonoscopy,
+                            boolean dentalCheck,
+                            boolean glaucomaScreening,
+                            boolean managingChronicCondition,
+                            boolean bloodPressue,
+                            boolean bloodGlucose,
+                            boolean cholestrol,
+                            boolean weightAssesment,
+                            boolean nonSmokersDeclaration) 
+{
+    
+}
--- src/main/java/za/co/bbd/pointscalculator/repository/PointsObjectRepository.java
+++ src/main/java/za/co/bbd/pointscalculator/repository/PointsObjectRepository.java
@@ -3,7 +3,7 @@ package za.co.bbd.pointscalculator.repository;
 import org.springframework.stereotype.Service;
 
 import za.co.bbd.pointscalculator.model.HealthyFoods;
-import za.co.bbd.pointscalculator.model.RequestHealthChecks;
+import za.co.bbd.pointscalculator.model.HealthCheck;
 @Service
 public class PointsObjectRepository {
     public HealthyFoods findFoodPoints(boolean dietitianVisit, int healthyFoodPerMonth){
@@ -30,5 +30,45 @@ public class PointsObjectRepository {
         }
 
         return new HealthyFoods(foodPoints, dietitianPoints);
+    }
+    
+    public HealthCheck findHealthCheckPoints( boolean vitalityAge,
+                                                int mentalWellbeing,
+                                                int golfPlayed,
+                                                boolean fluVaccine,
+                                                boolean initialHIVTest,
+                                                boolean annualHIVTest,
+                                                boolean papSmear,
+                                                boolean mammogram,
+                                                boolean colonoscopy,
+                                                boolean dentalCheck,
+                                                boolean glaucomaScreening,
+                                                boolean managingChronicCondition,
+                                                boolean bloodPressue,
+                                                boolean bloodGlucose,
+                                                boolean cholestrol,
+                                                boolean weightAssesment,
+                                                boolean nonSmokersDeclaration){
+
+                                                    
+        return new HealthCheck(
+            vitalityAge,
+            mentalWellbeing,
+            golfPlayed,
+            fluVaccine,
+            initialHIVTest,
+            annualHIVTest,
+            papSmear,
+            mammogram,
+            colonoscopy,
+            dentalCheck,
+            glaucomaScreening,
+            managingChronicCondition,
+            bloodPressue,
+            bloodGlucose,
+            cholestrol,
+            weightAssesment,
+            nonSmokersDeclaration
+        );
     }
 }
