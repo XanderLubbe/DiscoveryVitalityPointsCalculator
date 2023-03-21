54185639860703c6bc02b3d83ce8aef99f641bed
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2023-03-21 12:00:41 +0200

    Added HealthCheck Service, including logic

--- src/main/java/za/co/bbd/pointscalculator/model/HealthCheck.java
+++ src/main/java/za/co/bbd/pointscalculator/model/HealthCheck.java
@@ -1,22 +1,18 @@
 package za.co.bbd.pointscalculator.model;
 
-public record HealthCheck(boolean vitalityAge,
-                            int mentalWellbeing,
-                            int golfPlayed,
-                            boolean fluVaccine,
-                            boolean initialHIVTest,
-                            boolean annualHIVTest,
-                            boolean papSmear,
-                            boolean mammogram,
-                            boolean colonoscopy,
-                            boolean dentalCheck,
-                            boolean glaucomaScreening,
-                            boolean managingChronicCondition,
-                            boolean bloodPressue,
-                            boolean bloodGlucose,
-                            boolean cholestrol,
-                            boolean weightAssesment,
-                            boolean nonSmokersDeclaration) 
+public record HealthCheck(  int vitalityAgePoints,
+                            int mentalWellbeingPoints,
+                            int golfPlayedPoints,
+                            int fluVaccinePoints,
+                            int initialHIVTestPoints,
+                            int annualHIVTestPoints,
+                            int papSmearPoints,
+                            int mammogramPoints,
+                            int colonoscopyPoints,
+                            int dentalCheckPoints,
+                            int glaucomaScreeningPoints,
+                            int managingChronicConditionPoints,
+                            int vitalityCheckPoints) 
 {
     
 }
