d9fe3e3ab28288638a8e452d83a02e10ed3a7039
Xander Lubbe <x.lubbe@icloud.com>
2023-03-23 14:53:24 +0200

    Created Bands table and repo.

--- src/main/java/za/co/bbd/pointscalculator/repository/BandsRepository.java
+++ src/main/java/za/co/bbd/pointscalculator/repository/BandsRepository.java
@@ -2,13 +2,14 @@ package za.co.bbd.pointscalculator.repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import za.co.bbd.pointscalculator.model.BandsDTO;
 import za.co.bbd.pointscalculator.model.FitnessPointsDTO;
 
 import java.util.List;
 
-public interface BandsRepository extends CrudRepository<FitnessPointsDTO, Long>{
-
-    @Query ("SELECT t FROM FITNESS_POINTS t")
-    List<FitnessPointsDTO> getAllPoints();
+public interface BandsRepository extends CrudRepository<BandsDTO, Long>{
+    @Query("SELECT t FROM VITALITY_BANDS t WHERE t.lowerLimit <= :points AND t.upperLimit >= :points")
+    BandsDTO findVitalityBand(@Param("points") int points);
     
 }
