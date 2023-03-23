a23d04ef1e929a697b1dd0c993ea261dcb5b4984
AnneMienBBD <annas@bbd.co.za>
2023-03-23 19:09:29 +0200

    changed classes to records

--- src/main/java/za/co/bbd/pointscalculator/model/RequestHealthChecks.java
+++ src/main/java/za/co/bbd/pointscalculator/model/RequestHealthChecks.java
@@ -3,34 +3,22 @@ package za.co.bbd.pointscalculator.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
-@Getter
-public class RequestHealthChecks {
 
-    // General
-    private boolean vitalityAge;
-    private int mentalWellbeing;
-
-    // Exercise
-    private int golfPlayed;
-
-    // Further Health Checks
-    private boolean fluVaccine;			
-    private boolean initialHIVTest;			
-    private boolean annualHIVTest;			
-    private boolean papSmear;				
-    private boolean mammogram;					
-    private boolean colonoscopy;				
-    private boolean dentalCheck;				
-    private boolean glaucomaScreening;	
-    private boolean managingChronicCondition;
-
-    // Vitality Health Check (check if all are in range)
-    private boolean bloodPressure;
-	private boolean bloodGlucose;
-	private boolean cholestrol;
-	private boolean weightAssesment;
-	private boolean nonSmokersDeclaration;
-
-    
-}
+public record RequestHealthChecks (
+    boolean vitalityAge,
+    int mentalWellbeing,
+    int golfPlayed,
+    boolean fluVaccine,
+    boolean initialHIVTest,
+    boolean annualHIVTest,
+    boolean papSmear,
+    boolean mammogram,
+    boolean colonoscopy,
+    boolean dentalCheck,
+    boolean glaucomaScreening,
+    boolean managingChronicCondition,
+    boolean bloodPressure,
+	boolean bloodGlucose,
+	boolean cholestrol,
+	boolean weightAssesment,
+	boolean nonSmokersDeclaration){}
